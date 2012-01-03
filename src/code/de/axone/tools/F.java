90d7f5af2db129d315a8364eb2ad18ca7310a06a
hg <devnull@localhost>
2012-01-03 10:38:29 +0100

    Klasse F (format) und Base64Tests

---
+++ src/code/de/axone/tools/F.java
@@ -0,0 +1,147 @@
+package de.axone.tools;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class F {
+
+	static String simplifyClassName( String className ){
+		
+		int oldIndex = className.indexOf( '.' );
+		int index=oldIndex;
+		
+		if( oldIndex > 0 ){
+			while( ( index = className.indexOf( '.', index+1 ) ) > 0 ){
+				
+				oldIndex = index;
+			}
+			return className.substring( oldIndex+1 );
+		} else {
+			return className;
+		}
+	}
+
+	public static String ormat( Object o ){
+		StringBuilder r = new StringBuilder();
+		format( r, o );
+		return r.toString();
+	}
+
+	private static void formatCollection( StringBuilder r, Collection<?> l ){
+		
+		r.append( "(" );
+		boolean first = true;
+		for( Object o : l ){
+			if( first ) first = false;
+			else r.append( ", " );
+			
+			formatItem( r, o );
+		}
+		r.append( ")" );
+	}
+
+	private static void formatMap( StringBuilder r, Map<?,?> m ){
+		
+		r.append( "{" );
+		boolean first = true;
+		for( Object key : m.keySet() ){
+			if( first ) first = false;
+			else r.append( ", " );
+			
+			formatItem( r, key );
+			r.append( "=>" );
+			formatItem( r, m.get( key ) );
+		}
+		r.append( "}" );
+	}
+
+	private static void formatArray( StringBuilder r, Object a ){
+		
+		Class<?> component = a.getClass().getComponentType();
+		
+		r.append( "[" );
+		boolean first = true;
+		if( component == boolean.class ){
+			for( boolean o : (boolean[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == byte.class ){
+			for( byte o : (byte[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == char.class ){
+			for( char o : (char[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == short.class ){
+			for( short o : (byte[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == int.class ){
+			for( int o : (int[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == long.class ){
+			for( long o : (long[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == float.class ){
+			for( float o : (float[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else if( component == double.class ){
+			for( double o : (double[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		} else {
+			// This handles arrays of arrays, too
+			for( Object o : (Object[])a ){
+				_formatArrayItem( r, first, o );
+				first = false;
+			}
+		}
+		r.append( "]" );
+	}
+	
+	private static void _formatArrayItem( StringBuilder r, boolean first, Object o ){
+		if( ! first ) r.append( ", " );
+		formatItem( r, o );
+	}
+	
+	private static void format( StringBuilder r, Object o ){
+		
+		if( o == null ) r.append( S._NULL_ );
+		else if( o instanceof String ) formatString( r, (String)o );
+		else if( o instanceof Collection<?> ) formatCollection( r, (Collection<?>)o );
+		else if( o instanceof Map<?,?> ) formatMap( r, (Map<?,?>) o );
+		else if( o.getClass().isArray() ) formatArray( r, o );
+		else r.append( o );
+	}
+	
+	private static void formatString( StringBuilder r, String s ){
+		r.append( s );
+	}
+	
+	private static void formatItem( StringBuilder r, Object o ){
+		
+		boolean isSpecial = 
+				o == null ||
+				o instanceof Collection<?> ||
+				o instanceof Map<?,?> ||
+				o.getClass().isArray()
+		;
+		
+		if( !isSpecial ) r.append( '\'' );
+		format( r, o );
+		if( !isSpecial ) r.append( '\'' );
+	}
+
+}
---
+++ src/test/de/axone/tools/Base64Test.java
@@ -0,0 +1,32 @@
+package de.axone.tools;
+
+import static org.testng.Assert.*;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+@Test( groups="tools.base64" )
+public class Base64Test {
+
+	public void testBase64(){
+		
+		BASE64Encoder enc = new BASE64Encoder();
+		BASE64Decoder dec = new BASE64Decoder();
+		
+		Random rand = new Random();
+		
+		for( int i=0; i<1024; i++ ){
+			
+			byte [] dataIn = new byte[i];
+			rand.nextBytes( dataIn );
+			
+			String encS = enc.encode( dataIn );
+			
+			byte [] dataOut = dec.decode( encS );
+			
+			assertEquals( dataIn, dataOut );
+		}
+	}
+	
+}
