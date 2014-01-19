1e73edb113104f685104d3bbd3890ed468de4b94
Florian Bantner <f.bantner@axon-e.de>
2014-01-19 17:06:18 +0100

    Merge fixes

--- src/code/de/axone/tools/BASE64Encoder.java
+++ src/code/de/axone/tools/BASE64Encoder.java
@@ -10,20 +10,4 @@ package de.axone.tools;
  * 
  * @deprecated
  */
-<<<<<<< HEAD
 public class BASE64Encoder extends Base64ApacheCommons {}
-=======
-public class BASE64Encoder {
-	
-	public String encodeURLSafe( byte[] data ){
-		return Base64ApacheCommons.EncodeURLSafe( data );
-	}
-	
-	public String encodeURLSafe( String data ){
-		return encodeURLSafe( data.getBytes( Charsets.UTF8 ) );
-	}
-	
-	
-	
-}
->>>>>>> 457e3b271681ab21721ed66d420a3797d46c30e7
